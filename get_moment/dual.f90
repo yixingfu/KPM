aa20a277a93f3bc6dbccd92b03eb4368abb1d4df
Yixing Fu <yxfu93@hotmail.com>
2018-07-17 12:24:54 -0400

    HC_D3

--- get_moment/dual.f90
+++ get_moment/dual.f90
@@ -5,7 +5,8 @@
             ! a b
             ! c d
             ! [a1 a2]
-            ! [dual_a1 dual_a2]
+            ! [dual_a1
+            !  dual_a2]
 
             a = a1(1)
             b = a2(1)
@@ -15,8 +16,8 @@
             det_a = a*d-b*c
 
             dual_a1(1) = d
-            dual_a1(2) = -c
-            dual_a2(1) = -b
+            dual_a2(1) = -c
+            dual_a1(2) = -b
             dual_a2(2) = a
 
             dual_a1 = dual_a1/det_a
